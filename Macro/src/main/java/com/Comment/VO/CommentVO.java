4d8201d1e3ffd7eb4ca54f2ab2f74ef5fd180743
KimDongHyun <akxldk2@naver.com>
2020-02-06 00:31:44 +0900

    2020/02/02 김동현 댓글 작성시간 추가

--- Macro/src/main/java/com/Comment/VO/CommentVO.java
+++ Macro/src/main/java/com/Comment/VO/CommentVO.java
@@ -1,13 +1,23 @@
 package com.Comment.VO;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CommentVO {
+    //업데이트 시간(현재시간)
+    SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+    Calendar cal = Calendar.getInstance();
+    String today = formatter.format(cal.getTime());
+
     String content;
     String nickname;
     int article_id;
     int comment_id;
     int candidate_id;
     String social_id;
-
+    Timestamp create_time;
+    Timestamp update_time=Timestamp.valueOf(today);
     public void setComment(int candidate_id, int article_id,String content,String nickname,String social_id){
         this.candidate_id=candidate_id;
         this.article_id=article_id;
@@ -62,5 +72,21 @@ public class CommentVO {
 
     public void setSocial_id(String social_id) {
         this.social_id = social_id;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+
+    public Timestamp getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Timestamp update_time) {
+        this.update_time = update_time;
     }
 }
